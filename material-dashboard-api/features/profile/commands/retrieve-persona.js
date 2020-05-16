26342d9b21cbd6eb592e7f5344cdbd58a4dad5a5
Maria Vasquez <maria.ines.vf@gmail.com>
2020-05-16 15:33:52 -0600

    get persona function

---
+++ material-dashboard-api/features/profile/commands/retrieve-persona.js
@@ -0,0 +1,35 @@
+const {getPersona } = require('../repository');
+const { UPDATE_INFO_SUCCESS_MESSAGE, UPDATE_INFO_ERROR_MESSAGE } = require('../constants');
+
+
+
+async function loadPersona(req, res) {
+  let user = {};
+  const {
+    user: { id },
+  } = req;
+  const profileSuccessMessage = UPDATE_INFO_SUCCESS_MESSAGE;
+  try {
+    user = await getPersona(id);
+  } catch (error) {
+    user = error;
+  }
+  if (user.email) {
+    req.session.messages = { success: profileSuccessMessage };
+    req.session.userInfo = { ...user };
+    return res.send({
+      success: true,
+      userInfo: { ...user },
+      messages: { success: profileSuccessMessage },
+    });
+  }
+
+  const databaseError = UPDATE_INFO_ERROR_MESSAGE;
+
+  return res.send({
+    success: false,
+    messages: { errors: { databaseError } },
+  });
+} 
+
+module.exports = loadPersona;
--- material-dashboard-api/features/profile/routes.js
+++ material-dashboard-api/features/profile/routes.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 
 const requestBodyValidation = require('./commands/verify-request-body');
 const updateUserInfo = require('./commands/update-user-info');
+const retrievePersona = require('./commands/retrieve-persona');
 
 router.post('/update-profile-info', wrap(requestBodyValidation), wrap(updateUserInfo));
+router.get('/retrieve-persona/:id', wrap(requestBodyValidation), wrap(retrievePersona));
 
 module.exports = router;
